2eeaf6459c00e6d9c814084292ffe7301de4bf88
bljessica <1194620082@qq.com>
2021-01-03 00:28:42 +0800

    modify JZ13

--- 剑指offer/JZ13.调整数组顺序使奇数位于偶数前面.js
+++ 剑指offer/JZ13.调整数组顺序使奇数位于偶数前面.js
@@ -1,10 +1,24 @@
 function reOrderArray(array)
 {
     // write code here
-    let arr1 = [], arr2 = [];
-    for(let item of array) {
-        if(item % 2 === 1) arr1.push(item);
-        else arr2.push(item);
+    // 1
+    // let arr1 = [], arr2 = [];
+    // for(let item of array) {
+    //     if(item % 2 === 1) arr1.push(item);
+    //     else arr2.push(item);
+    // }
+    // return arr1.concat(arr2);
+
+    // 2
+    const len = array.length
+    for(let i = len - 1; i > 0; i--) {
+        for(let j = 0; j < i; j++) {
+            if (array[j] % 2 === 0 && array[j + 1] % 2 === 1) {
+                let tmp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = tmp
+            }
+        }
     }
-    return arr1.concat(arr2);
+    return array
 }
